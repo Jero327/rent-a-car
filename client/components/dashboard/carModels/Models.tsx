6fca42a9b53bab42b80dd2111de89b5e80fa3fbb
Cong Guo <jeraviah@gmail.com>
2023-12-15 12:13:57 +1300

    Client: allow user to add new car model

--- client/components/dashboard/carModels/Models.tsx
+++ client/components/dashboard/carModels/Models.tsx
@@ -1,9 +1,11 @@
+import AddModel from "./AddModel"
 import ModelsList from "./ModelsList"
 
 function Models() {
   return (
     <>
       <h1>This is models page</h1>
+      <AddModel />
       <ModelsList />
     </>
   )
