8b4507eeb75d48d668e05ef2c2a6674eb8402157
Cong Guo <jeraviah@gmail.com>
2023-12-15 10:46:27 +1300

    Update client dashboard page contents

--- client/components/dashboard/Dashboard.tsx
+++ client/components/dashboard/Dashboard.tsx
@@ -3,8 +3,8 @@ import { Link, Outlet } from 'react-router-dom'
 function Dashboard() {
   return (
     <>
-      <h1>This is Dashboard page</h1>
-      <Link to={`/dashboard/models`}>models</Link>
+      <h1>This is Dashboard HomePage</h1>
+      <Link to={`/dashboard/models`}>Manage Models</Link>
       <Outlet />
     </>
   )
