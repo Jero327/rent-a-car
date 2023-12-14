cf767f8eee0500e897ff16b0b7e06fa9f6da057d
Cong Guo <jeraviah@gmail.com>
2023-12-14 20:44:44 +1300

    Rename folder name

--- client/routes.tsx
+++ client/routes.tsx
@@ -2,7 +2,7 @@ import { Route, createRoutesFromElements } from 'react-router-dom'
 import Layout from './components/Layout'
 import HomePage from './components/HomePage'
 import Dashboard from './components/dashboard/Dashboard'
-import Models from './components/dashboard/models/Models'
+import Models from './components/dashboard/carModels/Models'
 
 export default createRoutesFromElements(
   <Route path="/" element={<Layout />}>
