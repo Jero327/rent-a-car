f12ab9a7e919cd03d4446d963163f660905e699c
Cong Guo <jeraviah@gmail.com>
2023-12-15 21:11:41 +1300

    Client: init dashboard locations component

---
+++ client/components/dashboard/locations/Locations.tsx
@@ -0,0 +1,14 @@
+// import AddModel from "./AddModel"
+// import ModelsList from "./ModelsList"
+
+function Locations() {
+  return (
+    <>
+      <h1>This is locations page</h1>
+      {/* <AddModel />
+      <ModelsList /> */}
+    </>
+  )
+}
+
+export default Locations
