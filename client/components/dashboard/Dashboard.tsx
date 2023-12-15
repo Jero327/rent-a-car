05165b9c25f1912cb3f0d97f05d3d77692ea40ec
Cong Guo <jeraviah@gmail.com>
2023-12-15 18:31:15 +1300

    Client: protect dashboard page by only be accessed by admin account

--- client/components/dashboard/Dashboard.tsx
+++ client/components/dashboard/Dashboard.tsx
@@ -1,11 +1,28 @@
 import { Link, Outlet } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function Dashboard() {
+  const { user, isLoading } = useAuth0()
+
+  if (isLoading) {
+    return <div>Loading ...</div>
+  }
+
   return (
     <>
       <h1>This is Dashboard HomePage</h1>
-      <Link to={`/dashboard/models`}>Manage Models</Link>
-      <Outlet />
+
+      {user?.sub === 'auth0|657aa87d5284b2ee5d38be4b' ? (
+        <>
+          <h2>Admin User: {user.name}</h2>
+          <Link to={`/dashboard/models`}>Manage Models</Link>
+          <Outlet />
+        </>
+      ) : (
+        <>
+          <div>Please use admin account to log in!</div>
+        </>
+      )}
     </>
   )
 }
