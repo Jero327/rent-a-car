a67e2ca5df2d67f914e493a71242b6f7782767f6
Cong Guo <jeraviah@gmail.com>
2023-12-19 16:37:06 +1300

    Client: add insert new car products component

---
+++ client/components/dashboard/carProducts/AddCarProduct.tsx
@@ -0,0 +1,84 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useState } from 'react'
+import { addCarProduct } from '../../../client_api/carProducts'
+import { useAuth0 } from '@auth0/auth0-react'
+import { newCarProduct } from '../../../../type/carProducts'
+
+function AddCarProduct() {
+  const { getAccessTokenSilently } = useAuth0()
+  const [isAddCarProduct, setIsAddCarProduct] = useState(false)
+
+  function handleAddNewCarProduct() {
+    setIsAddCarProduct(!isAddCarProduct)
+  }
+
+  const queryClient = useQueryClient()
+
+  async function insertCarProduct(c: newCarProduct) {
+    const accessToken = await getAccessTokenSilently()
+    await addCarProduct(c, accessToken)
+  }
+
+  const mutation = useMutation({
+    mutationFn: (carProduct: newCarProduct) => insertCarProduct(carProduct),
+    onSuccess: () => queryClient.invalidateQueries(['carProducts']),
+  })
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const form = new FormData(e.currentTarget)
+
+    const rego_number = form.get('rego_number') as string
+    const model = Number(form.get('model')) as number
+    const location = Number(form.get('location')) as number
+    const daily_rate = Number(form.get('daily_rate')) as number
+    const is_available = form.get('is_available') === 'true'
+
+    const newCarProduct = {
+      rego_number: rego_number,
+      model_id: model,
+      location_id: location,
+      daily_rate: daily_rate,
+      is_available: is_available,
+    }
+
+    mutation.mutate(newCarProduct)
+    e.currentTarget.reset()
+  }
+
+  return (
+    <>
+      <h2>This is add new car product page</h2>
+      <button onClick={handleAddNewCarProduct}>Add new car product</button>
+      <br />
+      <br />
+      {isAddCarProduct && (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Rego number:
+            <input name="rego_number" type="text" required></input>
+          </label>
+          <label>
+            Model:
+            <input name="model" type="number" required></input>
+          </label>
+          <label>
+            Location:
+            <input name="location" type="number" required></input>
+          </label>
+          <label>
+            Daily rate:
+            <input name="daily_rate" type="number" required></input>
+          </label>
+          <label>
+            Available?
+            <input name="is_available" type="checkbox"></input>
+          </label>
+          <input type="submit" value="Add"></input>
+        </form>
+      )}
+    </>
+  )
+}
+
+export default AddCarProduct
--- client/components/dashboard/carProducts/CarProducts.tsx
+++ client/components/dashboard/carProducts/CarProducts.tsx
@@ -1,9 +1,11 @@
+import AddCarProduct from './AddCarProduct'
 import CarProductsList from './CarProductsList'
 
 function CarProducts() {
   return (
     <>
       <h1>This is Car Products page</h1>
+      <AddCarProduct />
       <CarProductsList />
     </>
   )
