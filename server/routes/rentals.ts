79c302042e86d2c489198f13ab0c690f5ee2aa95
Cong Guo <jeraviah@gmail.com>
2023-12-23 19:15:24 +1300

    Server: expose getAllRentalsInfo API

--- server/routes/rentals.ts
+++ server/routes/rentals.ts
@@ -2,6 +2,7 @@ import express from 'express'
 
 import * as db from '../db/db_functions/rentals'
 import { validateAccessToken } from '../auth0'
+import checkAdmin from './checkAdmin'
 
 const router = express.Router()
 
@@ -22,6 +23,19 @@ router.get('/', validateAccessToken, async (req, res) => {
   const auth0Id = req.auth?.payload.sub as string
   try {
     const response = await db.getRentalsInfo(auth0Id)
+
+    res.status(200).json(response)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+router.get('/allrentalsinfo', validateAccessToken, async (req, res) => {
+  checkAdmin(req)
+  
+  try {
+    const response = await db.getAllRentalsInfo()
 
     res.status(200).json(response)
   } catch (error) {
